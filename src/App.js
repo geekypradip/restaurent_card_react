3d0ac3e5283b7e5878d8fa8a8a8fce1e088d6621
Pradip Mandal <54275322+geekypradip@users.noreply.github.com>
2021-12-31 13:06:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import RestaurantList from "./Component/RestaurantList";
+
+export default function App() {
+  return (
+    <div className="App">
+      <RestaurantList />
+    </div>
+  );
+}
